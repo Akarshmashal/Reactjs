0cf50832a3f9069c92019e83b2ddc9f5a74f01b9
Akarshmashal <mashalakarsh@gmail.com>
2025-04-08 09:34:59 +0530

    Updates

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const heading1 = React.createElement("h1", {id:"title"}, "Heading 1");
+const heading1 = React.createElement("h1", { id: "title", key: "h1" }, "Heading 1");
+const heading2 = React.createElement("h2", { id: "title", key: "h2" }, "Heading 2");
 
-const heading2 = React.createElement("h2", {id:"title"}, "Heading 2");
+const container = React.createElement("div", { id: "container" }, [heading1, heading2]);
 
-const container = React.createElement("div", {id:"container"},[heading1 ,heading2])
-
-const root = ReactDOM.createRoot(document.getElementById("root")); 
-
+const root = createRoot(document.getElementById("root"));
 root.render(container);
 
 
